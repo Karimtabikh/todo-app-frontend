21846f6b0e8dcf46f122617363a42c3341f1d538
Karimtabikh <karim.tabikh@outlook.com>
2025-01-25 13:46:55 +0200

    feat: Signin and Signup pages with react-hooj-form, shadcn and zod validation

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import { Lock, Mail, User } from "lucide-react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const schema = z.object({
+  email: z.string().email({ message: "Email is required" }),
+  password: z.string().min(3, { message: "Password is required" }),
+});
+
+type Schema = z.infer<typeof schema>;
+
+export default function SignInPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Schema>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<Schema> = (data) => {
+    console.log("data ", data);
+  };
+
+  return (
+    <div className="bg-white p-6 sm:p-8 rounded-lg shadow-lg w-full max-w-xs sm:max-w-sm md:max-w-md">
+      <h1 className="text-2xl font-bold mb-6 text-center">Sign Up</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <div className="relative">
+            <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <Input
+              id="email"
+              {...register("email")}
+              type="email"
+              placeholder="Enter your email"
+              className="pl-10"
+              required
+            />
+          </div>
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email.message}</p>
+          )}
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="password">Password</Label>
+          <div className="relative">
+            <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <Input
+              id="password"
+              {...register("password")}
+              type="password"
+              placeholder="Enter your password"
+              className="pl-10"
+              required
+            />
+          </div>
+          {errors.password && (
+            <p className="text-red-500 text-sm">{errors.password.message}</p>
+          )}
+        </div>
+        <input type="hidden" name="action" value="signup" />
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+      </form>
+
+      <div className="mt-4 text-center">
+        <Button variant="link" className="text-sm">
+          Don't have an account? Sign up
+        </Button>
+      </div>
+    </div>
+  );
+}
